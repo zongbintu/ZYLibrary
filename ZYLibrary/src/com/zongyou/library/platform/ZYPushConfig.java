2877b96be3bc9d23a1ffe99867f887c99469074d
ldh <394380623@qq.com>
2015-11-18 17:02:34 +0800

    添加统计与推送

--- ZYLibrary/src/com/zongyou/library/platform/ZYPushConfig.java
+++ ZYLibrary/src/com/zongyou/library/platform/ZYPushConfig.java
@@ -2,8 +2,15 @@ package com.zongyou.library.platform;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
+
+import com.zongyou.library.util.storage.PreferenceUtils;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import cn.jpush.android.api.JPushInterface;
+import cn.jpush.android.api.TagAliasCallback;
 
 /**
  * User: ldh (394380623@qq.com)
@@ -13,12 +20,55 @@ import cn.jpush.android.api.JPushInterface;
  */
 public class ZYPushConfig {
 
+    private static int num = 0;
 
-    public static void init(@NonNull Context context){
+    public static void init(@NonNull Context context) {
         JPushInterface.init(context);
     }
 
-    public static void setDebugMode(boolean debug){
+    public static void setDebugMode(boolean debug) {
         JPushInterface.setDebugMode(debug);
+    }
+
+    /**
+     * 标签
+     */
+    public static void initTags(@NonNull Context context, @NonNull Set<String> list) {
+
+
+        JPushInterface.setTags(context, list,
+                new TagAliasCallback() {
+
+                    @Override
+                    public void gotResult(int arg0, String arg1,
+                                          Set<String> arg2) {
+
+                    }
+                });
+    }
+
+    /**
+     * 别名
+     */
+    public static void initAlias(@NonNull final Context context, @NonNull final String alias) {
+
+        JPushInterface.setAlias(context, alias,
+                new TagAliasCallback() {
+
+                    @Override
+                    public void gotResult(int arg0, String arg1,
+                                          Set<String> arg2) {
+                        if (arg0 == 6002 && num < 3) {
+                            initAlias(context, alias);
+                            num++;
+                        } else {
+                            num = 0;
+                        }
+                    }
+                });
+    }
+
+    public static void clearAllNotifications(Context context) {
+        JPushInterface.clearAllNotifications(context);
     }
 }
